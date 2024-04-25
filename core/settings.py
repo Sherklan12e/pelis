3f53a3ab981ac77f39ba0e65df9b927e756e8d33
sherklan12e <davismendoza.08@gmail.com>
2024-04-25 08:26:52 -0300

    a

--- core/settings.py
+++ core/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'pulzRfFPCx13',
+        'HOST': 'ep-noisy-frost-a2x64nnu.eu-central-1.pg.koyeb.app',
+        'PORT': '5432', 
     }
 }
 
