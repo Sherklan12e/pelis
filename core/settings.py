de4cc41dfabd4f5a36c62d12b223f36c6582a54a
sherklan12e <davismendoza.08@gmail.com>
2024-04-25 22:04:33 -0300

    asdgf

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-ENCRYPT_KEY =b'r7Y-Wtm51P_vAwm77Nbh8tIgCRcYf0-hjVdCbQFhtvA='
+ENCRYPT_KEY =b'joJiMPlNvPCaXDJ2ibURtKCp8I7O3lM0NJs8aoJSgxA='
 # SECURITY WARNING: don't run with debug turned     on in production!
 DEBUG =False
 
